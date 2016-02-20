e9c2f58c8d9867462a7d7a76655cdea6d14323fa
xtcNet <kappaxdxtc@gmail.com>
2016-02-20 03:37:28 -0600

    Game is working!
    TODO: Networking, GUI

--- src/JACCGames/Hexles/Resources/Resources.java
+++ src/JACCGames/Hexles/Resources/Resources.java
@@ -6,17 +6,21 @@ package JACCGames.Hexles.Resources;
 import java.util.*;
 import org.newdawn.slick.*;
 public class Resources {
-    private static Map<String, Image> images;
-    private static Map<String, SpriteSheet> sprites;
-    private static Map<String, Sound> sounds;
+    public static Map<String, Image> images;
+    public static Map<String, SpriteSheet> sprites;
+    public static Map<String, Sound> sounds;
     public Resources(){
         images = new HashMap<String, Image>();
         sprites = new HashMap<String, SpriteSheet>();
         sounds = new HashMap<String, Sound>();
-
         try {
+            images.put("hex", loadImage("res/Hex.png"));
+            images.put("deadHex", loadImage("res/DeadHex.png"));
+            images.put("midHex", loadImage("res/MidHex.png"));
             images.put("bg", loadImage("res/bg.png"));
-        } catch (SlickException e) {
+            sounds.put("click", loadSound("res/Click.ogg"));
+            sounds.put("endgame", loadSound("res/Victory.ogg"));
+        } catch (SlickException e){
             e.printStackTrace();
         }
     }
@@ -25,5 +29,8 @@ public class Resources {
         return new Image(path, false, Image.FILTER_NEAREST);
     }
 
+    private Sound loadSound(String path) throws SlickException{
+        return new Sound(path);
+    }
 
 }
