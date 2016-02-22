0666f57e0f23afe7c4fc1843a65f51a14e0c4e7a
xtcNet <kappaxdxtc@gmail.com>
2016-02-21 21:49:11 -0600

    Networking started...
    TODO: Mobile, Finish Networking

---
+++ src/JACCGames/Hexles/Networking/PacketMessage.java
@@ -0,0 +1,9 @@
+package JACCGames.Hexles.Networking;
+
+/**
+ * Created by corey on 2/21/2016.
+ */
+// PacketMessage class. Everything in this class can be sent over the network.
+public class PacketMessage {
+    public String message;
+}
